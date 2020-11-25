807f04503a5f0af6ac2b978c90a4f0ca73ed557e
n3xem <yukyanjpn@gmail.com>
2020-11-25 20:53:16 +0900

    add: map全画面化 + 重複観測点抽出

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def map():
 
 @app.route("/result", methods=["GET"])
 def result():
+    overlap_observ_points = []
+    observ_point_rep_num = {}
     tweet_urls = list(set(request.args.getlist("name")))
     tweet_urls = [url for url in tweet_urls if re.match(
         r'^https://twitter.com/.*/status/([0-9]{10,})$', url)]
@@ -25,12 +27,19 @@ def result():
     quake_dics = []
     for n in tweet_urls:
         tweetid = feft.tweeturl2id(n)
-        if tweetid == 0:
-            continue
         tweet_time = feft.tweetid2time(tweetid)
-        quake_dics.append(tweet_time2earthquake(tweet_time))
+        earthquake = tweet_time2earthquake(tweet_time)
+        quake_dics.append(earthquake)
+        for intensity in earthquake["intensities"]:
+            observ_point_rep_num.setdefault(
+                intensity["observ_point_num"], {"num": 0, "latitude": intensity["latitude"], "longitude": intensity["longitude"]})
+            observ_point_rep_num[intensity["observ_point_num"]]["num"] += 1
 
-    return render_template("result.html", quake_dics=quake_dics, tweet_urls=tweet_urls)
+    for key in observ_point_rep_num.keys():
+        if observ_point_rep_num[key]["num"] >= len(tweet_urls):
+            overlap_observ_points.append(observ_point_rep_num[key])
+
+    return render_template("result.html", quake_dics=quake_dics, tweet_urls=tweet_urls, overlap_observ_points=overlap_observ_points)
 
 
 if __name__ == '__main__':
