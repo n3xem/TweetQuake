1c83d8a2e0be8748012e969b335206432e397269
n3xem <yukyanjpn@gmail.com>
2020-11-25 19:26:12 +0900

    add: 複数ツイート＆チェックボックス機能追加

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import folium
 import find_earthquake_from_tweet as feft
+import re
 from flask import Flask, request, render_template
 
 from find_earthquake_from_tweet import tweet_time2earthquake
@@ -18,12 +18,19 @@ def map():
 
 @app.route("/result", methods=["GET"])
 def result():
-    name = request.args.get("name")
-    tweetid = feft.tweeturl2id(name)
-    tweet_time = feft.tweetid2time(tweetid)
-    quake_dic = tweet_time2earthquake(tweet_time)
+    tweet_urls = list(set(request.args.getlist("name")))
+    tweet_urls = [url for url in tweet_urls if re.match(
+        r'^https://twitter.com/.*/status/([0-9]{10,})$', url)]
 
-    return render_template("result.html", quake_dic=quake_dic)
+    quake_dics = []
+    for n in tweet_urls:
+        tweetid = feft.tweeturl2id(n)
+        if tweetid == 0:
+            continue
+        tweet_time = feft.tweetid2time(tweetid)
+        quake_dics.append(tweet_time2earthquake(tweet_time))
+
+    return render_template("result.html", quake_dics=quake_dics, tweet_urls=tweet_urls)
 
 
 if __name__ == '__main__':
